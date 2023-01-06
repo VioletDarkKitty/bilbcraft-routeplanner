414a51a229a8664fa6260d91efa9087d3ad059c6
Lily Rivers <LilyRivers48@gmail.com>
2023-01-06 16:10:29 +0000

    Add requirements.txt and fix venv build

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 -m nuitka --standalone main.py --enable-plugin=numpy --enable-plugin=pyside6 --follow-imports --prefer-source-code --plugin-enable=pylint-warnings -o main.out --onefile
+. venv/bin/activate
+pip3 install nuitka
+pip3 install orderedset
+pip3 install zstandard
+python3 -m nuitka --standalone main.py --enable-plugin=numpy --enable-plugin=pyside6 --nofollow-import-to=tkinter --follow-imports --prefer-source-code --plugin-enable=pylint-warnings -o main.out --onefile
